d46e2246a6f151707b8b1df9442d920029e7aadb
Ananda Zaka <anandazaka23@gmail.com>
2025-07-09 08:45:39 +0700

    Up animation using AOS

--- opimol-process/resources/views/layouts/app.blade.php
+++ opimol-process/resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <link rel="icon" type="image/png" href="{{ asset('images/opimol-logo.jpg') }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
     <script>
         tailwind.config = {
@@ -145,6 +146,10 @@
 
         // Initialize on page load
         handleScrollAnimation();
+    </script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
     </script>
 </body>
 </html>
