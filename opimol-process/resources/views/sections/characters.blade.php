c49a899482c15161db86461041a26d03280fbe82
Ananda Zaka <anandazaka23@gmail.com>
2025-07-13 19:26:27 +0700

    update

--- opimol-process/resources/views/sections/characters.blade.php
+++ opimol-process/resources/views/sections/characters.blade.php
@@ -10,18 +10,18 @@
                 Inilah para otak di balik OPIMOL. Merekalah yang mengembangkan komunitas ini sampai serame sekarang. <span class="text-orange-500"> All hail to OPIMOL Admin</span>🙇‍♂️🙇‍♀️
             </p>
         </div>
-        <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
+        <div data-aos="zoom-in" data-aos-delay="500" class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
             @foreach($characters as $character)
-            <div class="bg-gray-800 p-6 rounded-lg object-center border border-gray-700 text-center hover:border-orange-400 transition-all hover:scale-105">
-                <div class="relative w-32 h-32 rounded-3xl mx-auto mb-4 rounded-3xl rounded-full">
-                    <img src="{{ asset('images/characters/' . $character['image']) }}" alt="{{ $character['name'] }}" class="w-full h-full rounded-3xl object-cover">
+                <div class="bg-gray-800 p-6 rounded-lg object-center border border-gray-700 text-center hover:border-orange-400 transition-all hover:scale-105">
+                    <div class="relative w-32 h-32 rounded-3xl mx-auto mb-4 rounded-3xl rounded-full">
+                        <img src="{{ asset('images/characters/' . $character['image']) }}" alt="{{ $character['name'] }}" class="w-full h-full rounded-3xl object-cover">
+                    </div>
+                    <h3 class="text-xl font-bold mb-2">{{ $character['name'] }}
+                        <i class="fas fa-check-circle text-blue-500 text-xl"></i>
+                    </h3>
+                    <p class="text-gray-300 mb-2">{{ $character['role'] }}</p>
+                    <p class="text-sm text-gray-400">{{ $character['description'] }}</p>
                 </div>
-                <h3 class="text-xl font-bold mb-2">{{ $character['name'] }}
-                    <i class="fas fa-check-circle text-blue-500 text-xl"></i>
-                </h3>
-                <p class="text-gray-300 mb-2">{{ $character['role'] }}</p>
-                <p class="text-sm text-gray-400">{{ $character['description'] }}</p>
-            </div>
             @endforeach
         </div>
         <div class="text-center mt-12">
